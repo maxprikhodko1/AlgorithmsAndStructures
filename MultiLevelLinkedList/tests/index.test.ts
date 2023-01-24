3f91c5786568eaca4ad6eca10857c7cbaf109139
maxymprikhodko <maxymprikhodko@gmail.com>
2023-01-24 11:15:55 +0300

    Delete index test

--- MultiLevelLinkedList/tests/index.test.ts
+++
@@ -1,7 +0,0 @@
-import { getString } from '../src/index';
-
-describe('testing index file', () => {
-    test('empty string should result in zero', () => {
-        expect(getString('abc')).toBe('Hello World abc');
-    });
-});
