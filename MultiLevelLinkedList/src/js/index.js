96afe7e1e6610a99ab31d52a6301d83d08e8fcfb
maxymprikhodko <maxymprikhodko@gmail.com>
2023-01-25 15:35:32 +0300

    Uncommit wrongly commited files

--- MultiLevelLinkedList/src/js/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getString = void 0;
-function getString(str) {
-    return 'Hello world ' + str;
-}
-exports.getString = getString;
-console.log(getString('abc'));
-//# sourceMappingURL=index.js.map
